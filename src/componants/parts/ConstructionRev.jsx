a12f486cc1a98dc3f2f7d47a23106b858ff82e60
Pritesh_khare <Priteshkhare6@gmail.com>
2025-12-21 11:11:43 +0530

    pattern svg name fix

--- src/componants/parts/ConstructionRev.jsx
+++ src/componants/parts/ConstructionRev.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ConstractionRev = () => {
   return (
     <div className="absolute">
-      <div className="bg-[url('pattern.svg')] bg-center h-22 border-t-5 border-b-5 flex items-center justify-center w-full animate-slide scale-x-[-1] -translate-x-2/5">
+      <div className="bg-[url('/pattern.svg')] bg-center h-22 border-t-5 border-b-5 flex items-center justify-center w-full animate-slide scale-x-[-1] -translate-x-2/5">
         
         <div className="whitespace-nowrap flex">
           
