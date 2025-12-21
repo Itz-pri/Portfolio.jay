bb3703656d18c81e18b6e9b28cfeec9ca06792f2
Pritesh_khare <Priteshkhare6@gmail.com>
2025-12-21 10:47:35 +0530

    pattern svg fix

--- src/componants/parts/Construction.jsx
+++ src/componants/parts/Construction.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Constraction = () => {
   return (
     <div className="absolute">
-      <div className="bg-[url('pattern.svg')] bg-center h-22 border-t-5 border-b-5 flex items-center justify-center w-full animate-slide">
+      <div className="bg-[url('/pattern.svg')] bg-center h-22 border-t-5 border-b-5 flex items-center justify-center w-full animate-slide">
         
         <div className="whitespace-nowrap flex">
           
